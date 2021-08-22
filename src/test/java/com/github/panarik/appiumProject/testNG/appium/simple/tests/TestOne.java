c5ba0b6ec133dd2a01f5d0d87b99126d56a49561
panarik <panarik@yandex.ru>
2021-08-23 00:26:37 +0300

    add Appium test with testNG

--- src/test/java/com/github/panarik/appiumProject/testNG/appium/simple/tests/TestOne.java
+++ src/test/java/com/github/panarik/appiumProject/testNG/appium/simple/tests/TestOne.java
@@ -4,6 +4,8 @@ import com.github.panarik.appiumProject.testNG.appium.simple.BaseClass;
 import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.Test;
 
+import static com.github.panarik.appiumProject.testNG.appium.simple.util.Util.waitForElement;
+
 public class TestOne extends BaseClass {
 
     @Test
@@ -15,30 +17,4 @@ public class TestOne extends BaseClass {
         waitForElement(5, recyclerView);
         driver.findElementById(recyclerView).isDisplayed();
     }
-
-    private void waitForElement(int seconds, String elementPath) {
-        for (int s = 0; s<seconds+1; s++) {
-            try {
-                driver.findElementById(elementPath);
-            } catch (NoSuchElementException e) {
-                System.out.println("waiting "+s+" seconds for: "+elementPath);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException interrupt) {
-                    interrupt.printStackTrace();
-                }
-            }
-        }
-    }
-
-
-    private boolean isElementPresent(String elementPath) {
-        try {
-            driver.findElementById(elementPath);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
 }
