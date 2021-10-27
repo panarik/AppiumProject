a3e8dd119ed5460e45a70a9d90fe2b87f8bdfd67
panarik <panarik@yandex.ru>
2021-10-27 23:52:12 +0300

    add selectors and matchers

--- src/test/java/com/github/panarik/appiumProject/SimpleTest.java
+++ src/test/java/com/github/panarik/appiumProject/SimpleTest.java
@@ -1,32 +1,53 @@
 package com.github.panarik.appiumProject;
 
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Assertions;
+
+import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class SimpleTest {
 
     @BeforeEach
     public void init() {
         open("https://www.google.com/");
+        element(byName("q")).setValue("selenide").pressEnter();
+    }
+
+
+    @Test
+    public void selectors() {
+        $("body").should(visible); //body
+        $("#main").should(visible); //find by id
+        $(".gLFyf.gsfi").should(visible); //find by class name
+        $("[role='main']"); //find by attribute
+        $("div[class='eKjLze'] div.yuRUbf").shouldHave(visible); //child
+//        $("a:not(href^='http')") // href не начинается с http
+
+
     }
 
     @Test
-    public void logo_visible() {
-        Assertions.assertTrue(element(byClassName("lnXdpd")).isDisplayed(), "Google logo has not found by className lnXdpd");
-        Assertions.assertTrue(element(byClassName("gb_f")).isDisplayed(), "Google logo has not found by className gb_f");}
+    public void matchers() {
+        $(".Q8LRLc").shouldHave(matchText("Россия")); //match
+        $(".mfMhoc").shouldHave(text("запросы")); //contains
+//        $(".LC20lb.DKV0Md").shouldHave(selectedText("Selenide: лаконичные и стабильные UI тесты на Java")); //has selected
+//        $(".yuRUbf").shouldHave(href("https://selenide.org"));
+    }
 
     @Test
-    public void search_test() {
-        element(byName("q")).setValue("selenide").pressEnter();
-        elements("#search .g").shouldHave(size(11));
+    public void elements_test() {
+        ElementsCollection result = elements("#search .g");
+        result  .shouldHave(sizeGreaterThanOrEqual(9))
+                .first().shouldHave(text("Selenide: лаконичные и стабильные UI тесты на Java"));
+        result.first().find(".r>a").click();
+
+        Wait().until(titleIs("Selenide"));
     }
 
 }
