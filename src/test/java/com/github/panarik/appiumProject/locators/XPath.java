5fc2d94eff72f02b7ab78842a490a2988ef86a82
panarik <panarik@yandex.ru>
2021-11-12 16:14:21 +0300

    add xPath locators

---
+++ src/test/java/com/github/panarik/appiumProject/locators/XPath.java
@@ -0,0 +1,21 @@
+package com.github.panarik.appiumProject.locators;
+
+public class XPath {
+
+    //Single
+    private static final String PARAMETER_TEXT_EXACT = "//*[@text='some text']"; //by text
+    private static final String PARAMETER_TEXT_CONTAINS = "//*[contains(@text,'some text')]"; //by text
+
+    //or
+    private static final String TEXT_OR_LABEL = "//*[@text='foo' or @label='foo']";
+
+    //list position
+    private static final String POSITION_NUMBER = "//*[@class='android.widget.FrameLayout'][1]"; //2-th position on list FrameLayout views
+
+    //descend
+    private static final String DESCEND = "//*[@name='foo']//*[@name='bar']";
+    private static final String DESCEND_NEAREST = "//*[@name='foo']/*[@name='bar']";
+
+    //combine
+    private static final String COMBINE = "//*[@class='someName' or @label='someLabel'][2]/*[@test='some text']"; //find view with text some text in nearest parent. Parent has 3-th view in RecycleView
+}
