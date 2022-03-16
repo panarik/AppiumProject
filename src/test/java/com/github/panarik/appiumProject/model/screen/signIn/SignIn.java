30167eb6da08e870761c7649be7262989327ac22
panarik <hM9yRMWCcLrk4vH>
2022-03-17 00:48:10 +0300

    Fix test.

--- src/test/java/com/github/panarik/appiumProject/model/screen/signIn/SignIn.java
+++ src/test/java/com/github/panarik/appiumProject/model/screen/signIn/SignIn.java
@@ -3,12 +3,9 @@ package com.github.panarik.appiumProject.model.screen.signIn;
 import com.github.panarik.appiumProject.controller.Controller;
 import com.github.panarik.appiumProject.model.base.MobileItem;
 
-/**
- * Класс в котором реализована работа экрана 'SignIn' для Android.
- */
 public class SignIn extends Controller {
 
-    private final MobileItem ITEM = new MobileItem("Поле", "XPATH", "//Button");
+    private final MobileItem ITEM = new MobileItem("Field", "xpath", "//android.widget.Button[@text='SIGN UP']");
 
     public SignIn checkScreen() {
         getElement(ITEM);
--- src/test/java/com/github/panarik/appiumProject/tests/base/BaseTestClass.java
+++ src/test/java/com/github/panarik/appiumProject/tests/base/BaseTestClass.java
@@ -6,20 +6,19 @@ import com.github.panarik.appiumProject.model.screen.signIn.SignIn;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-/**
- * Класс в котором запускается первоначальные методы и завершающие тесты методы
- */
+
 public class BaseTestClass {
 
-    AppiumInstance instance;
+    private AppiumInstance instance;
 
     //Setup pages
+    // Add only common pages. If page is a part of scenario do not create it.
     protected SignIn signIn = new SignIn();
     protected Menu menu = new Menu();
 
     @BeforeTest
     public void start() {
-        String osName = "ANDROID"; // Можно сделать переключение на iOS.
+        String osName = "ANDROID";
         System.out.println(osName + " driver: setup");
         instance = new AppiumInstance();
         instance.setup(osName);
